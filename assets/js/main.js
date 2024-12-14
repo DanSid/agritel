995ca6a8202481e22a16c691333ba2aa073ac0c3
Dan Sidsaya <dansidsaya@gmail.com>
2024-12-14 13:38:02 +0000

    Contact Us Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,7 +213,16 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     });
 });
 
-
+// Sending mail
+function sendMail(){
+  let params={
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value
+  }
+  emailjs.send(("service_60h1ace", "template_pzu2oke", params).then(alert("Email Message sent successfully")))
+}
 
 
   /**
